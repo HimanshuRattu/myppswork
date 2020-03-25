c7be78586fab6198bd292f0a54a77c4901d4a541
Himanshu Rattu <53931499+HimanshuRattu@users.noreply.github.com>
2020-03-25 19:05:36 +0530

    Update Pascal_tri.c

--- Pascal_tri.c
+++ Pascal_tri.c
@@ -1,18 +1,20 @@
 #include<stdio.h>
+#include<conio.h>
 int main()
 {
-    int i, space, j, k=0;
-    printf("Enter number of rows: ");
-    scanf("%d", &j);
-    for (i=1; i<=j; ++i,k=0) {
-        for (space=1; space<=j-i; ++space)
-        { printf("  "); }
-        while (k!=2*i-1) 
-        {
-            printf("* ");
-            ++k;
-        }
-        printf("\n");
-    }    
-    return 0;
+  int i, space, j, k=0;
+  printf("Enter number of rows: ");
+  scanf("%d", &j);
+  for (i=1; i<=j; ++i,k=0) {
+      for (space=1; space<=j-i; ++space)
+      { printf("  "); }
+      while (k!=2*i-1) 
+      {
+          printf("* ");
+          ++k;
+      }
+      printf("\n");
+  }    
+  getch();
+  return 0;
 }
